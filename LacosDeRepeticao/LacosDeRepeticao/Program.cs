bba0cb71d1c1434304c2648f56f0cfd6adafafda
Sss41 <silvana.souza63@gmail.com>
2019-09-12 09:56:44 -0300

    Commit

---
+++ LacosDeRepeticao/LacosDeRepeticao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_LacosDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            while (count < 1000000)
+            {
+                Console.WriteLine("Número: " + count);
+                count++;
+            }
+            Console.WriteLine("terminou o loop");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LacosDeRepeticao/OndasEstrturadas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OndasEstrturadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caracter = "/***/";
+            var caracterCresce = $"\\***\\";
+            while (true)
+            {
+                for (int i = 0; i <= 200; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("\'");
+                    }
+                    Console.WriteLine($"{caracterCresce}");
+                }
+                for (int i = 200; i >= 0; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("\'");
+                    }
+                    Console.WriteLine($"{caracter}");
+                }
+            }
+        }
+    }
+}
