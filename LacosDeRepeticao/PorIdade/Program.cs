8f2b21e50e52b6cd31979579ed6550fe2ff7383f
vilsonmoro <vilsonmoro@gmail.com>
2019-09-13 13:58:43 -0300

    Commit

---
+++ LacosDeRepeticao/PorIdade/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramExecution();
+        }
+
+        private static void ProgramExecution()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Qual sua idade? : ");
+                int idade = Convert.ToInt16(Console.ReadLine());
+                bool situacao = VerifyAge(idade);
+                String mensagem = SetMessageForSituation(situacao);
+                Console.WriteLine(mensagem);
+                Console.Write("Tentar novamente? (1 para sim e 2 para não) : ");
+                string interesse = Convert.ToString(Console.ReadKey().KeyChar);
+                switch (interesse)
+                {
+                    case "1":
+                        Console.Clear();
+                        break;
+                    case "2":
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static bool VerifyAge(int idade) => idade >= 18;
+
+        private static String SetMessageForSituation(bool situacao) => situacao ? "Hoje é sexta, bora beber" : "Tem leitinho na geladeira";
+        
+    }
+}
---
+++ LacosDeRepeticao/Split/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetCountOfWord("a"));
+        }
+
+        private static int GetCountOfWord(String palavra) => GetTextContent().Split(palavra).Length - 1;
+
+        private static String GetTextContent() => @"Pensando mais a longo prazo, a execução dos pontos do programa estimula a padronização dos modos de operação convencionais.O empenho em analisar a constante divulgação das informações acarreta um processo de reformulação e modernização dos relacionamentos verticais entre as hierarquias.No entanto, não podemos esquecer que o fenômeno da Internet faz parte de um processo de gerenciamento dos níveis de motivação departamental.A certificação de metodologias que nos auxiliam a lidar com o entendimento das metas propostas apresenta tendências no sentido de aprovar a manutenção do sistema de participação geral.Do mesmo modo, o novo modelo estrutural aqui preconizado garante a contribuição de um grupo importante na determinação do fluxo de informações.";
+
+    }
+}
